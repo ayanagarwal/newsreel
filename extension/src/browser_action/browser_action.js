08b781ebc7034bd838dc72c72852a5e8305736e6
Vinesh Kannan <vinesh@omnipointment.com>
2017-11-05 07:07:43 -0600

    Add chrome extension:

--- extension/src/browser_action/browser_action.js
+++ extension/src/browser_action/browser_action.js
@@ -1,3 +1,19 @@
+let config = {
+	apiKey: "AIzaSyCsTL7ZOxNOAUByHQVgGAUIW6OcjXbaCDQ",
+	authDomain: "borkedin.firebaseapp.com",
+	databaseURL: "https://borkedin.firebaseio.com",
+	projectId: "borkedin",
+	storageBucket: "borkedin.appspot.com",
+	messagingSenderId: "769825906375"
+};
+let FirebaseApp = firebase.initializeApp(config);
+let db = FirebaseApp.database();
+
+function getURLHash(url) {
+	let hash = url.split('//')[1].split('?')[0].replace(/\W+/g, '');
+	return hash;
+}
+
 const COMIC_URL = "https://clever-garage.glitch.me/comic";
 
 function getComicFromArticle(article) {
@@ -67,6 +83,21 @@ function renderComic(comic) {
 	});
 
 	output.appendChild(parent);
+
+	let btnDiv = document.createElement('div');
+		btnDiv.innerHTML = `
+			<div class="field is-grouped is-grouped-centered">
+				<p class="control">
+					<button data-action="save" class="button is-info">Save Comic</button>
+				</p>
+				<p class="control">
+					<a href="${comic.url}" target="blank_" class="button is-info is-outlined">View Article</a>
+				</p>
+			</div>
+			<br>
+		`;
+	output.appendChild(btnDiv);
+
 	return output;
 }
 
@@ -77,14 +108,39 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	console.log(request);
 	if (request.type === 'send_article') {
 		request.article.images = true;
-		getComicFromArticle(request.article).then((comic) => {
-			console.log(comic);
-			let div = renderComic(comic);
-			output.innerHTML = '';
-			output.appendChild(div);
-		}).catch((res) => {
-			console.log(res);
-			output.innerHTML = `<p class="title is-1">${res.error}</p>`;
+		let urlHash = getURLHash(request.article.url);
+		db.ref(`newsreel/comics/${urlHash}`).once('value', (snap) => {
+			let val = snap.val() || false;
+			if (val) {
+				let comic = val;
+				comic.url = request.article.url;
+				let div = renderComic(comic);
+				output.innerHTML = '';
+				output.appendChild(div);
+				let btn = output.querySelector('[data-action="save"]')
+					btn.addEventListener('click', (e) => {
+						let cidx = parseInt(btn.dataset.comic, 10);
+						let hash = getURLHash(comic.url);
+						db.ref(`newsreel/comics/${hash}`).set(comic);
+					});
+			} else {
+				getComicFromArticle(request.article).then((comic) => {
+					console.log(comic);
+					comic.url = request.article.url;
+					let div = renderComic(comic);
+					output.innerHTML = '';
+					output.appendChild(div);
+					let btn = output.querySelector('[data-action="save"]')
+						btn.addEventListener('click', (e) => {
+							let cidx = parseInt(btn.dataset.comic, 10);
+							let hash = getURLHash(comic.url);
+							db.ref(`newsreel/comics/${hash}`).set(comic);
+						});
+				}).catch((res) => {
+					console.log(res);
+					output.innerHTML = `<p class="title is-1">${res.error}</p>`;
+				});
+			}
 		});
 	}
 });
--- extension/src/browser_action/contentscripts.js
+++ extension/src/browser_action/contentscripts.js
@@ -6,17 +6,17 @@ function sendMessage(data, callback){
 	});
 }
 
-let title = document.querySelector('h1').innerText;
-let content = Array.from(document.querySelectorAll('p')).map(d => d.innerText).reduce((a, v) => a + v);
-let article = {
-	title: title,
-	content: content
-};
-console.log(article);
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	console.log(request);
 	if (request.type === 'get_article') {
+		let title = document.querySelector('h1').innerText;
+		let content = Array.from(document.querySelectorAll('p')).map(d => d.innerText).reduce((a, v) => a + v);
+		let article = {
+			title: title,
+			content: content,
+			url: window.location.href
+		};
+		console.log(article);
 		sendMessage({
 			type: 'send_article',
 			article: article
